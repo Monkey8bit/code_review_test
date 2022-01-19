b619a680715f18f4afa732c8282579c8cc543484
Monkey8bit <de_construct@mail.ru>
2022-01-19 21:54:38 +0400

    task_01_fix

--- task_1.py
+++ task_1.py
@@ -1,2 +1,2 @@
 words = input("Enter words, separated by space: ").split()
-print(*[word[:10] if len(word) > 10 else word for word in enumerate(words, start=1)], sep="\n")
+print(*[(row, word[:10]) if len(word) > 10 else (row, word) for row, word in enumerate(words, start=1)], sep="\n")
